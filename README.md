b25e18ad8fb0299c429a5d02edf841e64df4650f
Bowuigi <matucrimella@gmail.com>
2021-07-05 15:40:08 -0300

    Removed segfault on EOF

--- README.md
+++ README.md
@@ -27,8 +27,6 @@ It was made in 15 minutes so expect bugs such as:
 
 - Aliases defined at runtime do not work
 
-- The program segfaults when you send EOF
-
 ## Extra Features
 
 It has extra features such as:
--- rshw.c
+++ rshw.c
@@ -8,18 +8,25 @@
  * Adds filename completion, (emacs/vi) keybindings and cursor
  * 
  * At the cost of:
- * Segfaults on EOF
  * Runtime declared aliases do not work
 */
+
+char *nulltoexit(char *str) {
+	if (str==NULL)
+		return "exit";
+	return str;
+}
+
 int main(void) {
 	char cmd[1024]="ls";
 	char *prompt="rshw >";
 
 	while (1) {
-		strcpy(cmd,readline(prompt));
+		strcpy(cmd,nulltoexit(readline(prompt)));
 		if (!strcmp(cmd,"exit"))
 			break;
 		system(cmd);
-		add_history(cmd);
+		if (strcmp(cmd,""))
+			add_history(cmd);
 	}
 }
